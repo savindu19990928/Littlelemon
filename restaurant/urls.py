7549fd87940ef975e14a3d04bb7c1884a9dfe084
savindu19990928 <devriousteam@gmail.com>
2023-12-05 02:44:18 +0530

    Update urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
@@ -13,4 +14,5 @@ urlpatterns = [
     path('loginuser/', views.login, name="loginuser"),
     path('registeruser/', views.register, name="registeruser"),
     path('logoutuser/', views.logout, name="logoutuser"),
+    path('api-token-auth/', obtain_auth_token),
 ]
